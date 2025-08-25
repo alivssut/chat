91f1a94bbebe0bff495cb63a427d731b4d21053c
alivasisari <alivasisari1380@gmail.com>
2025-08-25 15:39:05 +0330

    Add user contact model

--- backend/c_accounts/admin.py
+++ backend/c_accounts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import User, UserProfile
+from .models import User, UserProfile, Contact
+from django.contrib.sessions.models import Session
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(Contact)
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('session_key', 'get_decoded', 'expire_date')
--- backend/c_accounts/models.py
+++ backend/c_accounts/models.py
@@ -4,8 +4,21 @@ import uuid
 # Create your models here.
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    contacts = models.ManyToManyField(
+        "self",
+        through="Contact",
+        symmetrical=False,
+        related_name="related_to",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "c_users"
+        indexes = [
+            models.Index(fields=["username"], name="idx_user_username"),
+            models.Index(fields=["email"], name="idx_user_email"),
+        ]
 
     def __str__(self):
         return self.username
@@ -16,6 +29,44 @@ class UserProfile(models.Model):
     bio = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "c_user_profiles"
 
     def __str__(self):
         return self.user.username
+    
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="my_contacts"
+    )
+
+    contact_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="in_contacts"
+    )
+
+    display_name  = models.CharField(max_length=150, blank=True, null=True)
+
+    is_blocked = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "c_user_contacts"
+        unique_together = ("owner", "contact_user")
+        indexes = [
+            models.Index(fields=["owner"], name="idx_contact_owner"),
+            models.Index(fields=["contact_user"], name="idx_contact_user"),
+            models.Index(fields=["display_name"], name="idx_contact_display_name"),
+        ]
+        ordering = ["display_name"]
+
+    def __str__(self):
+        return self.display_name or self.contact_user.username
